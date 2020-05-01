10e8723970da331a01c4119e86e1100d7c9365b1
ygy <zero0_1one@163.com>
2020-05-01 19:46:07 +0800

    1:添加 timeAgo 方法,   2:  format 支持

---
+++ test/time_test.js
@@ -0,0 +1,21 @@
+const time = require('../lib/time')
+const chai = require('chai')
+const expect = chai.expect
+chai.use(require('chai-like'))
+
+describe('timeTools_test', function () {
+  it.only('timeAgo', function () {
+    let now = Date.now()
+    expect(time.timeAgo(now)).to.be.equal('刚刚')
+    expect(time.timeAgo(now - 60 * 1000)).to.be.equal('1分钟前')
+    expect(time.timeAgo(now - 3 * 60 * 1000)).to.be.equal('3分钟前')
+
+    expect(time.timeAgo(now - time.HOUR_MS)).to.be.equal('1小时前')
+    expect(time.timeAgo(now - 2 * time.HOUR_MS)).to.be.equal('2小时前')
+    expect(time.timeAgo(now - time.DAY_MS)).to.be.equal('昨天')
+    expect(time.timeAgo(now - 2 * time.DAY_MS)).to.be.equal('前天')
+    expect(time.timeAgo(now - 3 * time.DAY_MS)).to.be.equal('3天前')
+    expect(time.timeAgo(now - 364 * time.DAY_MS)).to.be.equal('364天前')
+    expect(time.timeAgo(now - 365 * time.DAY_MS)).to.be.equal('1年前')
+  })
+})
